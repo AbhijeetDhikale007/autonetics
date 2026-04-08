90fd7ac9a27f6fc086049a09ffc2a3b85cc8acfb
AbhijeetDhikale007 <Abhijeetdhikale007@gmail.com>
2026-04-08 13:16:05 +0530

    Created Unocss Config File

---
+++ uno.config.ts
@@ -0,0 +1,36 @@
+import {
+    defineConfig,
+    presetAttributify,
+    presetIcons,
+    presetTypography,
+    presetUno,
+    presetWebFonts,
+    transformerDirectives,
+    transformerVariantGroup
+} from 'unocss'
+
+export default defineConfig({
+    shortcuts: [
+        // ...
+    ],
+    theme: {
+        colors: {
+            // ...
+        }
+    },
+    presets: [
+        presetUno(),
+        presetAttributify(),
+        presetIcons(),
+        presetTypography(),
+        presetWebFonts({
+            fonts: {
+                sans: 'Outfit:300,400,500,600,700',
+            },
+        }),
+    ],
+    transformers: [
+        transformerDirectives(),
+        transformerVariantGroup(),
+    ],
+})
