c35962698963697d41cfa4ce4b874f59e6faf396
AbhijeetDhikale007 <Abhijeetdhikale007@gmail.com>
2026-04-08 13:19:03 +0530

    Created: Body of Website

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import "virtual:uno.css";
+	import "../app.css";
+	import favicon from "$lib/assets/favicon.svg";
+	import { onMount } from "svelte";
+	import Lenis from "lenis";
+	import Header from "$lib/components/Header.svelte";
+
+	let { children } = $props();
+
+	onMount(() => {
+		const lenis = new Lenis({
+			autoRaf: true,
+		});
+	});
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<Header />
+
+<main class="min-h-screen">
+	{@render children()}
+</main>
