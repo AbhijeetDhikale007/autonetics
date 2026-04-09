c0e8785e331d5d01f4b12e0c4521559b20421515
AbhijeetDhikale007 <Abhijeetdhikale007@gmail.com>
2026-04-09 19:18:47 +0530

    Fixed: Heading Components, Used Props Method

--- src/lib/components/Heading.svelte
+++ src/lib/components/Heading.svelte
@@ -1,5 +1,5 @@
-<script lang='ts'>
-    export let HeadingTitle: string
+<script lang="ts">
+	let { HeadingTitle }: { HeadingTitle: string } = $props();
 </script>
 
 <h1 class="font-Arsenal text-4xl md:text-5xl font-extrabold text-white mb-6 tracking-tight">{HeadingTitle}</h1>
